6f71587b531b36ce3858f534726058f3cb9454f1
genaro gonzalez <genarrogg@gmail.com>
2023-10-29 21:20:39 -0400

    mi primer commit

---
+++ POO.js
@@ -0,0 +1,21 @@
+const hola = "hola mundo"
+let hola2 = "hola mundo"
+var hola3 = "hola mundo"
+
+const obtenerEdad = (dad) => {
+    const edad = dad
+
+    return edad
+}
+
+
+
+
+function holaMundo(){
+    console.log(hola)
+
+    return 
+    
+}
+
+console.log(holaMundo())
