c45b4ca8f67a18b9e1fe0c31e4d5f4e31880011a
genaro gonzalez <genarrogg@gmail.com>
2023-10-29 21:48:59 -0400

    hola

--- xys.js
+++ xys.js
@@ -1,4 +1,4 @@
-class Hombre {
+class Humano {
     constructor(nombre, edad) {
         this.nombre = nombre;
         this.edad = edad;
@@ -16,8 +16,41 @@ class Hombre {
         this.nombre = nombre;
     }
 
+
+    getEdad() {
+        return this.edad;
+    }
+
+    setEdad(edad) {
+        this.edad = edad;
+    }
+
+    toString() {
+        return `Nombre: ${this.nombre}, Edad: ${this.edad}`;
+    }
+}
+class Hombre extends Humano {
+    SEXO = 'Hombre';
+
+    constructor(nombre, edad) {
+        super(nombre, edad);
+    }
+
+    saludar() {
+        console.log(`Hola, soy ${this.SEXO} ${this.nombre}`);
+    }
+}
+
+class Mujer extends Humano{
+    SEXO = 'Mujer';
+
+    constructor(nombre, edad) {
+        super(nombre, edad);
+    }
 }
 
 const xys = new Hombre('xys', 20);
+const xys2 = new Mujer('xyssa', 22);
 
-console.log(xys.toString());
+console.log(xys.saludar());
+console.log(xys2.saludar());
